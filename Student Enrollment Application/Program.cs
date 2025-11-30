8ecb5b74bf8e8fbf95290c5128572cecdd44f5a0
nex-uri <wdtnjp27d9@privaterelay.appleid.com>
2025-12-01 04:58:25 +0800

    Add project files.

---
+++ Student Enrollment Application/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Student_Enrollment_Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
